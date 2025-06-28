9ed54750c2dc85349e45a2ca49169b3a36a9adbe
Etele Kovács <etikova777@gmail.com>
2025-06-29 01:27:13 +0200

    made mosquito counting simple cv2, saved result to csv and xlsx

---
+++ utils/oragnize.py
@@ -0,0 +1,28 @@
+import os
+import glob
+import shutil
+
+# unpack all the image files from the folders
+base_folders = ["../data/feher_1", "../data/feher_2", "../data/piros", "../data/zold", "../data/kek"]
+
+# inside the base folders there are subfolders with with subfolders
+# the second subfolders contain the images
+# move the images from the second subfolders to the base folder
+for base_folder in base_folders:
+    # get all the subfolders in the base folder
+    subfolders = os.listdir(base_folder)
+    subfolders = [os.path.join(base_folder, subfolder) for subfolder in subfolders if os.path.isdir(os.path.join(base_folder, subfolder))]
+
+    for subfolder in subfolders:
+        # get all the image files in the subfolder
+        subusubfolders = os.listdir(subfolder)
+        subusubfolders = [os.path.join(subfolder, subusubfolder) for subusubfolder in subusubfolders if os.path.isdir(os.path.join(subfolder, subusubfolder))]
+        
+        for subusubfolder in subusubfolders:
+            # get all the image files in the subusubfolder
+            image_files = glob.glob(os.path.join(subusubfolder, "*.*"))
+            for image_file in image_files:
+                # move the image file to the base folder
+                shutil.move(image_file, base_folder)
+            # remove the empty subusubfolder
+            os.rmdir(subusubfolder)
